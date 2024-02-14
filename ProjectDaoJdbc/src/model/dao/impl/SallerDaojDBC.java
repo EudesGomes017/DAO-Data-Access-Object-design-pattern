7a16bc30cf380228110d288eb3279f73830d25b1
EudesGomes017 <eudesgomes017@gmail.com>
2024-02-13 23:36:33 -0300

    Create class DaoFactory

---
+++ ProjectDaoJdbc/src/model/dao/impl/SallerDaojDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SallerDao;
+import model.entities.Saller;
+
+public class SallerDaojDBC implements SallerDao {
+
+	@Override
+	public void insert(Saller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Saller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Saller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Saller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
